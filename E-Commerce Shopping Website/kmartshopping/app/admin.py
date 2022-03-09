95ef99425104064c135e91903672252b8721403d
AyushVanpariya <ayushvanparia@gmail.com>
2022-03-09 09:05:14 +0530

    update

--- E-Commerce Shopping Website/kmartshopping/app/admin.py
+++ E-Commerce Shopping Website/kmartshopping/app/admin.py
@@ -1,18 +1,35 @@
 from django.contrib import admin
 from .models import Customer, Product, Cart, OrderPlaced
+from django.utils.html import format_html
+from django.urls import reverse
+
 
 @admin.register(Customer)
 class CustomerModelAdmin(admin.ModelAdmin):
-  list_display = ['id', 'user', 'name', 'locality', 'city', 'zipcode', 'state']
+    list_display = ['id', 'user', 'name',
+                    'locality', 'city', 'zipcode', 'state']
+
 
 @admin.register(Product)
 class ProductModelAdmin(admin.ModelAdmin):
-  list_display = ['id', 'title', 'selling_price','discounted_price', 'description', 'brand', 'category', 'product_image' ]
+    list_display = ['id', 'title', 'selling_price', 'discounted_price',
+                    'description', 'brand', 'category', 'product_image']
+
 
 @admin.register(Cart)
 class CartModelAdmin(admin.ModelAdmin):
-  list_display = ['id', 'user', 'product', 'quantity']
+    list_display = ['id', 'user', 'product', 'quantity']
+
 
 @admin.register(OrderPlaced)
 class OrderPlacedModelAdmin(admin.ModelAdmin):
-  list_display = ['id', 'user', 'customer', 'product', 'quantity', 'ordered_date', 'status']
+    list_display = ['id', 'user', 'customer', 'customer_info',
+                    'product', 'product_info', 'quantity', 'ordered_date', 'status']
+
+    def customer_info(self, obj):
+        link = reverse("admin:app_customer_change", args=[obj.customer.pk])
+        return format_html('<a href="{}">{}</a>', link, obj.customer.name)
+
+    def product_info(self, obj):
+        link = reverse("admin:app_product_change", args=[obj.product.pk])
+        return format_html('<a href="{}">{}</a>', link, obj.product.title)
--- E-Commerce Shopping Website/kmartshopping/kmartshopping/settings.py
+++ E-Commerce Shopping Website/kmartshopping/kmartshopping/settings.py
@@ -122,7 +122,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = '/profile/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
     
